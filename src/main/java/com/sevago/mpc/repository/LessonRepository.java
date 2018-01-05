fab7a65d8a9f63dbc4bfcbe583fc934585d76185
Seva Golovko <seva.golovko@gmail.com>
2018-01-05 15:27:55 -0800

    fix the failing build, remove weird inport from the LessonRepository class

--- src/main/java/com/sevago/mpc/repository/LessonRepository.java
+++ src/main/java/com/sevago/mpc/repository/LessonRepository.java
@@ -1,13 +1,12 @@
 package com.sevago.mpc.repository;
 
 import com.sevago.mpc.domain.Lesson;
-import org.h2.index.PageBtreeLeaf;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Repository;
-
-import org.springframework.data.jpa.repository.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
